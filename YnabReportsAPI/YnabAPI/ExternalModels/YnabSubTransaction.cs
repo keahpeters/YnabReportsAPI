6263a1c16c5298429897946eb4783637a8d580c6
Keah Peters <keahpeters@hotmail.co.uk>
2020-07-29 23:01:36 +0100

    Fixed external transaction model

---
+++ YnabReportsAPI/YnabAPI/ExternalModels/YnabSubTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace YnabReportsAPI.YnabAPI.ExternalModels
+{
+    public class YnabSubTransaction
+    {
+        public string Id { get; set; } = default!;
+
+        public int Amount { get; set; }
+
+        [JsonPropertyName("payee_name")]
+        public string? PayeeName { get; set; }
+
+        [JsonPropertyName("category_name")]
+        public string CategoryName { get; set; } = default!;
+
+        public bool Deleted { get; set; }
+    }
+}
