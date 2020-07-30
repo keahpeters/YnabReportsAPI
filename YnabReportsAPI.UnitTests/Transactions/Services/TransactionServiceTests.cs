7567653f6628265c8ef9e9c3959becab1bd10645
Keah Peters <keahpeters@hotmail.co.uk>
2020-07-30 11:59:27 +0100

    Added unit tests

--- YnabReportsAPI.UnitTests/Transactions/Services/TransactionServiceTests.cs
+++ YnabReportsAPI.UnitTests/Transactions/Services/TransactionServiceTests.cs
@@ -79,5 +79,109 @@ namespace YnabReportsAPI.UnitTests.Transactions.Services
 
             result.Should().BeEquivalentTo(expectedResult);
         }
+
+        [Test]
+        public async Task GetTransactions_YnabServiceReturnsDeletedTransactions_DeletedNonSplitTransactionsAreNotReturned()
+        {
+            List<YnabTransaction> ynabTransaction = new List<YnabTransaction>
+            {
+                new YnabTransaction { Date = new DateTime(2020, 1, 1), Amount = 10500, CategoryName = "Category1", Approved = true, Deleted = false, SubTransactions = new List<YnabSubTransaction>() },
+                new YnabTransaction { Date = new DateTime(2020, 1, 2), Amount = 50000, CategoryName = "Category2", Approved = true, Deleted = true, SubTransactions = new List<YnabSubTransaction>() },
+            };
+
+            this.ynabService.Setup(y => y.GetTransactions(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new YnabResponse { Data = new YnabData { Transactions = ynabTransaction } });
+
+            IEnumerable<Transaction> result = await this.transactionService.GetTransactions("testBudgetId", null);
+
+            List<Transaction> expectedResult = new List<Transaction>
+            {
+                new Transaction(new DateTime(2020, 1, 1), 10.5M, "Category1"),
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public async Task GetTransactions_YnabServiceReturnsTransactions_SplitTransactionsAreReturned()
+        {
+            List<YnabTransaction> ynabTransaction = new List<YnabTransaction>
+            {
+                new YnabTransaction { 
+                    Date = new DateTime(2020, 1, 1), 
+                    Amount = 10500, 
+                    Approved = true, 
+                    SubTransactions = new List<YnabSubTransaction>
+                    {
+                        new YnabSubTransaction { Amount = 10500, CategoryName = "Category1" },
+                        new YnabSubTransaction { Amount = 50000, CategoryName = "Category2" },
+                    }
+                },
+            };
+
+            this.ynabService.Setup(y => y.GetTransactions(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new YnabResponse { Data = new YnabData { Transactions = ynabTransaction } });
+
+            IEnumerable<Transaction> result = await this.transactionService.GetTransactions("testBudgetId", null);
+
+            List<Transaction> expectedResult = new List<Transaction>
+            {
+                new Transaction(new DateTime(2020, 1, 1), 10.5M, "Category1"),
+                new Transaction(new DateTime(2020, 1, 1), 50M, "Category2"),
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public async Task GetTransactions_YnabServiceReturnsUnapprovedTransactions_UnapprovedSplitTransactionsAreNotReturned()
+        {
+            List<YnabTransaction> ynabTransaction = new List<YnabTransaction>
+            {
+                new YnabTransaction {
+                    Date = new DateTime(2020, 1, 1),
+                    Amount = 10500,
+                    Approved = false,
+                    SubTransactions = new List<YnabSubTransaction>
+                    {
+                        new YnabSubTransaction { Amount = 10500, CategoryName = "Category1" },
+                        new YnabSubTransaction { Amount = 50000, CategoryName = "Category2" },
+                    }
+                },
+            };
+
+            this.ynabService.Setup(y => y.GetTransactions(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new YnabResponse { Data = new YnabData { Transactions = ynabTransaction } });
+
+            IEnumerable<Transaction> result = await this.transactionService.GetTransactions("testBudgetId", null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task GetTransactions_YnabServiceReturnsDeletedTransactions_DeletedSplitTransactionsAreNotReturned()
+        {
+            List<YnabTransaction> ynabTransaction = new List<YnabTransaction>
+            {
+                new YnabTransaction {
+                    Date = new DateTime(2020, 1, 1),
+                    Amount = 10500,
+                    Approved = true,
+                    SubTransactions = new List<YnabSubTransaction>
+                    {
+                        new YnabSubTransaction { Amount = 10500, CategoryName = "Category1", Deleted = false },
+                        new YnabSubTransaction { Amount = 50000, CategoryName = "Category2", Deleted = true },
+                    }
+                },
+            };
+
+            this.ynabService.Setup(y => y.GetTransactions(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new YnabResponse { Data = new YnabData { Transactions = ynabTransaction } });
+
+            IEnumerable<Transaction> result = await this.transactionService.GetTransactions("testBudgetId", null);
+
+            List<Transaction> expectedResult = new List<Transaction>
+            {
+                new Transaction(new DateTime(2020, 1, 1), 10.5M, "Category1"),
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
     }
 }
--- YnabReportsAPI/Transactions/Services/TransactionService.cs
+++ YnabReportsAPI/Transactions/Services/TransactionService.cs
@@ -41,17 +41,23 @@ namespace YnabReportsAPI.Transactions.Services
         }
 
         private IEnumerable<Transaction> GetSingleTransactions(IEnumerable<YnabTransaction> ynabTransactions)
-            => ynabTransactions.Where(x => x.Approved && !x.SubTransactions.Any()).Select(x => new Transaction(x.Date, this.ConvertMilliUnitsToDecimal(x.Amount), x.CategoryName));
+            => ynabTransactions
+            .Where(x => !x.Deleted && x.Approved && !x.SubTransactions.Any())
+            .Select(x => new Transaction(x.Date, this.ConvertMilliUnitsToDecimal(x.Amount), x.CategoryName));
 
         private IEnumerable<Transaction> GetSplitTransactions(IEnumerable<YnabTransaction> ynabTransactions)
         {
             var transactions = new List<Transaction>();
 
-            IEnumerable<YnabTransaction>? splitTransactions = ynabTransactions.Where(x => x.Approved && x.SubTransactions.Any());
+            IEnumerable<YnabTransaction>? splitTransactions = ynabTransactions
+                .Where(x => !x.Deleted && x.Approved && x.SubTransactions.Any());
 
             foreach (var splitTransaction in splitTransactions)
             {
-                transactions.AddRange(splitTransaction.SubTransactions.Select(x => new Transaction(splitTransaction.Date, this.ConvertMilliUnitsToDecimal(x.Amount), x.CategoryName)));
+                transactions.AddRange(
+                    splitTransaction.SubTransactions
+                    .Where(x => !x.Deleted)
+                    .Select(x => new Transaction(splitTransaction.Date, this.ConvertMilliUnitsToDecimal(x.Amount), x.CategoryName)));
             }
 
             return transactions;
